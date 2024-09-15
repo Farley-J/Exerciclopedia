b218096c2a0edc0776eaf8a624830a3c0164cde0
Farley Jeannis <farleyj008@gmail.com>
2024-09-15 14:49:09 -0400

    Update ExerciseDetails.jsx

--- website/src/pages/ExerciseDetails.jsx
+++ website/src/pages/ExerciseDetails.jsx
@@ -14,10 +14,10 @@ const ExerciseDetails = () => {
     <Stack gap="60px" sx={{ flexDirection: { lg: 'row' }, p: '20px', alignItems: 'center' }}>
       <img src={item.gifUrl} alt={item.name} loading="lazy" className="detail-image" />
       <Stack sx={{ gap: { lg: '35px', xs: '20px' } }}>
-        <Typography sx={{ fontSize: { lg: '64px', xs: '30px' } }} fontWeight={700} textTransform="capitalize">
+        <Typography sx={{ fontSize: { lg: '34px', xs: '20px' } }} fontWeight={700} textTransform="capitalize">
           {item.name}
         </Typography>
-        <Typography sx={{ fontSize: { lg: '24px', xs: '18px' } }} color="#4F4C4C">
+        <Typography sx={{ fontSize: { lg: '16px', xs: '12px' } }} color="#4F4C4C">
           <Typography variant='h5'>Target:<span> {item.target}</span></Typography><br></br>
           <Typography variant='h5'>Equipment:<span> {item.equipment}</span></Typography><br></br>
           <Typography variant='h5' sx={{ textDecoration: 'underline' }}>Instructions</Typography>
